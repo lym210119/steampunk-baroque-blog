df5fe6250efeea5b6bc5d3bca90e5b144b38c593
lym210119 <lym210119@gmail.com>
2026-03-13 23:56:02 +0800

    Update page.tsx

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -1,6 +1,6 @@
 import { getSortedPostsData } from '@/lib/posts'
 import PostCard from '@/components/PostCard'
-import ClientPagination from '@/components/ClientPagination' // 新建客户端组件
+import PaginationClient from '@/components/PaginationClient' // 新建客户端组件
 
 const POSTS_PER_PAGE = 5
 
@@ -10,7 +10,7 @@ export default function BlogHome() {
   return (
     <div className="max-w-4xl mx-auto py-12 px-4 relative gear-corner">
       <h1 className="text-5xl mb-12 border-b-4 border-brass-300 pb-4">最新文章</h1>
-      <ClientPagination posts={allPosts} postsPerPage={POSTS_PER_PAGE} />
+      <PaginationClient posts={allPosts} postsPerPage={POSTS_PER_PAGE} />
     </div>
   )
 }
